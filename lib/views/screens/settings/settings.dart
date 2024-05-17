e8e063a5927afd1f4d46c3e0025731bca2441139
im donghyeon <d.im@kinotrope.co.jp>
2024-05-17 12:32:59 +0900

    temporary settings screen

---
+++ lib/views/screens/settings/settings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Text(
+          'Settings Screen',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
