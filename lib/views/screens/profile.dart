e1fdc9e6d9a3297bec797478bb0243760868e652
“IM <dominiqueue7@gmail.com>
2024-05-19 12:44:40 +0900

    profile image edit icon

--- lib/views/screens/profile.dart
+++ lib/views/screens/profile.dart
@@ -31,101 +31,127 @@ class _ProfileState extends State<Profile> {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 60),
-              CircleAvatar(
-                backgroundImage: AssetImage(
-                  "assets/images/cm${random.nextInt(10)}.jpeg",
-                ),
-                radius: 50,
-              ),
-              SizedBox(height: 10),
-              Text(
-                names[random.nextInt(10)],
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22,
-                ),
-              ),
-              SizedBox(height: 3),
-              Text(
-                "Status should be here",
-                style: TextStyle(),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextButton(
-                    child: Icon(
-                      Icons.message,
-                      color: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 60),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage(
+                        "assets/images/cm${random.nextInt(10)}.jpeg",
+                      ),
+                      radius: 50,
                     ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Colors.grey,
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap: () {
+                          // 편집 아이콘 클릭 시 동작 추가
+                        },
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey[200],
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.black,
+                            size: 18,
+                          ),
+                        ),
                       ),
                     ),
-                    onPressed: () {},
-                  ),
-                  SizedBox(width: 10),
-                  TextButton(
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              SizedBox(height: 40),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 50),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    _buildCategory("Posts"),
-                    _buildCategory("Friends"),
-                    _buildCategory("Groups"),
                   ],
                 ),
-              ),
-              SizedBox(height: 20),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                primary: false,
-                padding: EdgeInsets.all(5),
-                itemCount: 15,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 200 / 200,
+                SizedBox(height: 10),
+                Text(
+                  names[random.nextInt(10)],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
                 ),
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: Image.asset(
-                      "assets/images/cm${random.nextInt(10)}.jpeg",
-                      fit: BoxFit.cover,
+                SizedBox(height: 3),
+                Text(
+                  "Status should be here",
+                  style: TextStyle(),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextButton(
+                      child: Icon(
+                        Icons.message,
+                        color: Colors.white,
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Colors.grey,
+                        ),
+                      ),
+                      onPressed: () {},
                     ),
-                  );
-                },
-              ),
-            ],
+                    SizedBox(width: 10),
+                    TextButton(
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      _buildCategory("Posts"),
+                      _buildCategory("Friends"),
+                      _buildCategory("Groups"),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  primary: false,
+                  padding: EdgeInsets.all(5),
+                  itemCount: 15,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 200 / 200,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Image.asset(
+                        "assets/images/cm${random.nextInt(10)}.jpeg",
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
