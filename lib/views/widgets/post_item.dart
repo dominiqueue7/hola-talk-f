b366ddf60c213f9f69aac727ab636aa55a8e52c3
im donghyeon <d.im@kinotrope.co.jp>
2024-07-03 17:32:10 +0900

    #24 Error fix & Increase readability

--- lib/views/widgets/post_item.dart
+++ lib/views/widgets/post_item.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:intl/intl.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:HolaTalk/views/screens/feeds/post_detail.dart'; // PostDetailPage를 임포트합니다.
+import 'package:HolaTalk/views/screens/feeds/post_detail.dart';
 
 class PostItem extends StatelessWidget {
+  // 클래스 속성 정의
   final String postId;
   final String userId;
   final String name;
@@ -12,10 +13,11 @@ class PostItem extends StatelessWidget {
   final String img;
   final String content;
   final String? userProfileUrl;
-  final int? maxLines; // maxLines 추가
+  final int? maxLines;
 
-  PostItem({
-    super.key,
+  // 생성자
+  const PostItem({
+    Key? key,
     required this.postId,
     required this.userId,
     required this.name,
@@ -23,10 +25,124 @@ class PostItem extends StatelessWidget {
     required this.img,
     required this.content,
     this.userProfileUrl,
-    this.maxLines, // maxLines 추가
-  });
+    this.maxLines,
+  }) : super(key: key);
 
-  String formatTime(DateTime dateTime) {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: InkWell(
+        onTap: () => _navigateToPostDetail(context),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildUserInfo(context),
+            if (img.isNotEmpty) _buildPostImage(context),
+            _buildPostContent(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 사용자 정보 위젯 구성
+  Widget _buildUserInfo(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: CircleAvatar(
+        backgroundColor: Colors.grey[200],
+        child: _buildProfileImage(),
+      ),
+      title: Text(
+        name,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+      ),
+      trailing: Text(
+        _formatTime(time),
+        style: const TextStyle(
+          fontWeight: FontWeight.w300,
+          fontSize: 11,
+        ),
+      ),
+    );
+  }
+
+  // 프로필 이미지 위젯 구성
+  Widget _buildProfileImage() {
+    if (userProfileUrl != null && userProfileUrl!.isNotEmpty) {
+      return CachedNetworkImage(
+        imageUrl: userProfileUrl!,
+        imageBuilder: (context, imageProvider) => CircleAvatar(
+          backgroundImage: imageProvider,
+        ),
+        placeholder: (context, url) => const CircularProgressIndicator(),
+        errorWidget: (context, url, error) => const Icon(Icons.person, size: 30.0, color: Colors.grey),
+      );
+    } else {
+      return const Icon(Icons.person, size: 30.0, color: Colors.grey);
+    }
+  }
+
+  // 게시물 이미지 위젯 구성
+  Widget _buildPostImage(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: img,
+      height: 170,
+      width: MediaQuery.of(context).size.width,
+      fit: BoxFit.cover,
+      placeholder: (context, url) => Center(
+        child: LoadingAnimationWidget.staggeredDotsWave(
+          color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.blue,
+          size: 50,
+        ),
+      ),
+      errorWidget: (context, url, error) => Container(
+        height: 170,
+        color: Colors.grey[300],
+        child: const Center(
+          child: Icon(Icons.error, color: Colors.red),
+        ),
+      ),
+    );
+  }
+
+  // 게시물 내용 위젯 구성
+  Widget _buildPostContent() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        content,
+        maxLines: maxLines,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+
+  // 게시물 상세 페이지로 이동
+  Future<void> _navigateToPostDetail(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PostDetailPage(
+          postId: postId,
+          userId: userId,
+          name: name,
+          time: time,
+          img: img,
+          content: content,
+        ),
+      ),
+    );
+    if (result == true) {
+      // TODO: 새로고침 로직을 여기에 추가할 수 있습니다.
+    }
+  }
+
+  // 시간 포맷 변환
+  String _formatTime(DateTime dateTime) {
     final now = DateTime.now();
     final difference = now.difference(dateTime);
 
@@ -38,91 +154,10 @@ class PostItem extends StatelessWidget {
       }
     } else {
       if (difference.inHours >= 1) {
-        return '${difference.inHours} hours ago'; // 1시간 이상
+        return '${difference.inHours} hours ago';
       } else {
-        return '${difference.inMinutes} minutes ago'; // 1시간 미만
+        return '${difference.inMinutes} minutes ago';
       }
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 5),
-      child: InkWell(
-        onTap: () async {
-          final result = await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PostDetailPage(
-                postId: postId,
-                userId: userId,
-                name: name,
-                time: time,
-                img: img,
-                content: content,
-              ),
-            ),
-          );
-          if (result == true) {
-            // 새로고침 로직을 여기에 추가할 수 있습니다.
-          }
-        },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.grey[200],
-                child: userProfileUrl != ''
-                    ? null
-                    : Icon(Icons.person, size: 30.0, color: Colors.grey),
-                backgroundImage: userProfileUrl != null
-                    ? CachedNetworkImageProvider(userProfileUrl!)
-                    : null,
-              ),
-              contentPadding: EdgeInsets.all(0),
-              title: Text(
-                name,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-              trailing: Text(
-                formatTime(time),
-                style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 11,
-                ),
-              ),
-            ),
-            if (img.isNotEmpty)
-              CachedNetworkImage(
-                imageUrl: img,
-                height: 170,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-                placeholder: (context, url) => Center(
-                  child: LoadingAnimationWidget.staggeredDotsWave(
-                    color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.blue,
-                    size: 50,
-                  ),
-                ),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-              ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                content,
-                maxLines: maxLines, // maxLines 설정
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
