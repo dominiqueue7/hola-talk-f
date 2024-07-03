571425eae0cabc172db5ee536719c483556bd694
im donghyeon <d.im@kinotrope.co.jp>
2024-07-03 18:42:06 +0900

    Increase readability

--- lib/views/screens/friends.dart
+++ lib/views/screens/friends.dart
@@ -12,6 +12,9 @@ class Friends extends StatefulWidget {
 
 class _FriendsState extends State<Friends>
     with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  static const int TAB_COUNT = 4;
+  static const List<String> TAB_TITLES = ["Followers", "Following", "Online", "Near"];
+
   late TabController _tabController;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   late String _currentUserId;
@@ -19,7 +22,7 @@ class _FriendsState extends State<Friends>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(vsync: this, initialIndex: 0, length: 4);
+    _tabController = TabController(vsync: this, initialIndex: 0, length: TAB_COUNT);
     _currentUserId = _auth.currentUser!.uid;
   }
 
@@ -28,10 +31,10 @@ class _FriendsState extends State<Friends>
       context: context,
       isScrollControlled: true,
       builder: (context) => DraggableScrollableSheet(
-        initialChildSize: 1.0, // 모달 열렸을때 얼마나 보여줄지 비율
-        minChildSize: 0.8, // 얼마나 내려야 닫히는지 비율
+        initialChildSize: 1.0,
+        minChildSize: 0.8,
         maxChildSize: 1.0,
-        expand: false, // 모달 윗부분 빈공간 제거
+        expand: false,
         builder: (context, scrollController) {
           return ProfilePage(
             userId: userId,
@@ -46,37 +49,12 @@ class _FriendsState extends State<Friends>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          decoration: InputDecoration.collapsed(
-            hintText: 'Search',
-          ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.filter_list),
-            onPressed: () {},
-          ),
-        ],
-        bottom: TabBar(
-          controller: _tabController,
-          indicatorColor: Theme.of(context).colorScheme.secondary,
-          labelColor: Theme.of(context).colorScheme.secondary,
-          unselectedLabelColor: Theme.of(context).textTheme.bodySmall?.color,
-          isScrollable: false,
-          tabs: <Widget>[
-            Tab(text: "Followers"),
-            Tab(text: "Following"),
-            Tab(text: "Online"),
-            Tab(text: "Near"),
-          ],
-        ),
-      ),
+      appBar: _buildAppBar(),
       body: TabBarView(
         controller: _tabController,
         children: <Widget>[
-          _buildFollowersList(context),
-          _buildFollowingList(context),
+          _buildUserList(context, 'followers'),
+          _buildUserList(context, 'following'),
           _buildOnlineList(context),
           Center(child: Text('Near')), // Placeholder for Near tab
         ],
@@ -88,29 +66,41 @@ class _FriendsState extends State<Friends>
     );
   }
 
-  Widget _buildFollowersList(BuildContext context) {
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: TextField(
+        decoration: InputDecoration.collapsed(hintText: 'Search'),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.filter_list),
+          onPressed: () {},
+        ),
+      ],
+      bottom: TabBar(
+        controller: _tabController,
+        indicatorColor: Theme.of(context).colorScheme.secondary,
+        labelColor: Theme.of(context).colorScheme.secondary,
+        unselectedLabelColor: Theme.of(context).textTheme.bodySmall?.color,
+        isScrollable: false,
+        tabs: TAB_TITLES.map((title) => Tab(text: title)).toList(),
+      ),
+    );
+  }
+
+  Widget _buildUserList(BuildContext context, String listType) {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('followers')
-          .doc(_currentUserId)
-          .collection('userFollowers')
-          .snapshots(),
+      stream: _getUserStream(listType),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: LoadingAnimationWidget.staggeredDotsWave(
-              color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.blue,
-              size: 50,
-            ),
-          );
+          return _buildLoadingIndicator(context);
         }
         if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(child: Text('No followers found.'));
+          return Center(child: Text('No $listType found.'));
         }
 
         List<Future<DocumentSnapshot>> userFutures = snapshot.data!.docs.map((doc) {
-          String followerId = doc.id;
-          return FirebaseFirestore.instance.collection('users').doc(followerId).get();
+          return FirebaseFirestore.instance.collection('users').doc(doc.id).get();
         }).toList();
 
         return FutureBuilder<List<DocumentSnapshot>>(
@@ -119,99 +109,29 @@ class _FriendsState extends State<Friends>
             if (!userSnapshots.hasData) {
               return Center(child: CircularProgressIndicator());
             }
-            var userDocs = userSnapshots.data!;
-            return ListView.separated(
-              padding: EdgeInsets.all(10),
-              separatorBuilder: (context, index) {
-                return Divider();
-              },
-              itemCount: userDocs.length,
-              itemBuilder: (context, index) {
-                var user = userDocs[index].data() as Map<String, dynamic>;
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    backgroundImage: user['profileImageUrl'] != null && user['profileImageUrl'].isNotEmpty 
-                        ? CachedNetworkImageProvider(user['profileImageUrl']) 
-                        : null,
-                    radius: 25,
-                    child: user['profileImageUrl'] == null || user['profileImageUrl'].isEmpty 
-                        ? Icon(Icons.person, size: 30.0, color: Colors.grey) 
-                        : null,
-                  ),
-                  title: Text(user['name'] ?? 'Unknown'),
-                  subtitle: Text(user['status'] ?? ''),
-                  onTap: () => _showUserProfile(userDocs[index].id),
-                );
-              },
-            );
+            return _buildUserListView(userSnapshots.data!);
           },
         );
       },
     );
   }
 
-  Widget _buildFollowingList(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
+  Stream<QuerySnapshot> _getUserStream(String listType) {
+    if (listType == 'followers') {
+      return FirebaseFirestore.instance
+          .collection('followers')
+          .doc(_currentUserId)
+          .collection('userFollowers')
+          .snapshots();
+    } else if (listType == 'following') {
+      return FirebaseFirestore.instance
           .collection('following')
           .doc(_currentUserId)
           .collection('userFollowing')
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: LoadingAnimationWidget.staggeredDotsWave(
-              color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.blue,
-              size: 50,
-            ),
-          );
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(child: Text('No following users found.'));
-        }
-
-        List<Future<DocumentSnapshot>> userFutures = snapshot.data!.docs.map((doc) {
-          String followingId = doc.id;
-          return FirebaseFirestore.instance.collection('users').doc(followingId).get();
-        }).toList();
-
-        return FutureBuilder<List<DocumentSnapshot>>(
-          future: Future.wait(userFutures),
-          builder: (context, userSnapshots) {
-            if (!userSnapshots.hasData) {
-              return Center(child: CircularProgressIndicator());
-            }
-            var userDocs = userSnapshots.data!;
-            return ListView.separated(
-              padding: EdgeInsets.all(10),
-              separatorBuilder: (context, index) {
-                return Divider();
-              },
-              itemCount: userDocs.length,
-              itemBuilder: (context, index) {
-                var user = userDocs[index].data() as Map<String, dynamic>;
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    backgroundImage: user['profileImageUrl'] != null && user['profileImageUrl'].isNotEmpty 
-                        ? CachedNetworkImageProvider(user['profileImageUrl']) 
-                        : null,
-                    radius: 25,
-                    child: user['profileImageUrl'] == null || user['profileImageUrl'].isEmpty 
-                        ? Icon(Icons.person, size: 30.0, color: Colors.grey) 
-                        : null,
-                  ),
-                  title: Text(user['name'] ?? 'Unknown'),
-                  subtitle: Text(user['status'] ?? ''),
-                  onTap: () => _showUserProfile(userDocs[index].id),
-                );
-              },
-            );
-          },
-        );
-      },
-    );
+          .snapshots();
+    } else {
+      throw ArgumentError('Invalid listType: $listType');
+    }
   }
 
   Widget _buildOnlineList(BuildContext context) {
@@ -222,44 +142,62 @@ class _FriendsState extends State<Friends>
           .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: LoadingAnimationWidget.staggeredDotsWave(
-              color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.blue,
-              size: 50,
-            ),
-          );
+          return _buildLoadingIndicator(context);
         }
         if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
           return Center(child: Text('No online users found.'));
         }
 
-        return ListView.separated(
-          padding: EdgeInsets.all(10),
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
-          itemCount: snapshot.data!.docs.length,
-          itemBuilder: (context, index) {
-            var user = snapshot.data!.docs[index].data() as Map<String, dynamic>;
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.white,
-                backgroundImage: user['profileImageUrl'] != null && user['profileImageUrl'].isNotEmpty 
-                    ? CachedNetworkImageProvider(user['profileImageUrl']) 
-                    : null,
-                radius: 25,
-                child: user['profileImageUrl'] == null || user['profileImageUrl'].isEmpty 
-                    ? Icon(Icons.person, size: 30.0, color: Colors.grey) 
-                    : null,
-              ),
-              title: Text(user['name'] ?? 'Unknown'),
-              subtitle: Text(user['status'] ?? ''),
-              onTap: () => _showUserProfile(snapshot.data!.docs[index].id),
-            );
-          },
-        );
+        return _buildUserListView(snapshot.data!.docs);
       },
     );
+  }
+
+  Widget _buildLoadingIndicator(BuildContext context) {
+    return Center(
+      child: LoadingAnimationWidget.staggeredDotsWave(
+        color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.blue,
+        size: 50,
+      ),
+    );
+  }
+
+  Widget _buildUserListView(List<DocumentSnapshot> users) {
+    return ListView.separated(
+      padding: EdgeInsets.all(10),
+      separatorBuilder: (context, index) => Divider(),
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        var user = users[index].data() as Map<String, dynamic>;
+        return _buildUserListTile(user, users[index].id);
+      },
+    );
+  }
+
+  Widget _buildUserListTile(Map<String, dynamic> user, String userId) {
+    return ListTile(
+      leading: _buildUserAvatar(user),
+      title: Text(user['name'] ?? 'Unknown'),
+      subtitle: Text(user['status'] ?? ''),
+      onTap: () => _showUserProfile(userId),
+    );
+  }
+
+  Widget _buildUserAvatar(Map<String, dynamic> user) {
+    return CircleAvatar(
+      backgroundColor: Colors.white,
+      backgroundImage: _getUserProfileImage(user),
+      radius: 25,
+      child: _getUserProfileImage(user) == null
+          ? Icon(Icons.person, size: 30.0, color: Colors.grey)
+          : null,
+    );
+  }
+
+  ImageProvider? _getUserProfileImage(Map<String, dynamic> user) {
+    return user['profileImageUrl'] != null && user['profileImageUrl'].isNotEmpty
+        ? CachedNetworkImageProvider(user['profileImageUrl'])
+        : null;
   }
 
   @override
